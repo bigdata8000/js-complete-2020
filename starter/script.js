cb6fd36c9151e30787df4c546dd3317e4809362a
Henry Phu-Henh Lam <50170584+henrymmmm@users.noreply.github.com>
2020-05-16 23:44:36 +0100

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,28 @@
+/*
+    Number: floating point numbers.
+    String: text.
+    Boolean: true or falase.
+    Undefined: data type with no value.
+    Null: empty.
+*/
+
+// console.log("Hello World 2020!");
+
+
+// video 2.5
+
+var firstName = "John";
+var lastName = "Smith";
+var age = 21;
+var canDrive = true;
+var job;                // should show undefined in console.
+var $favFood = Curry;   // starting var name with $ and _ .
+var _city = London;
+var 3children = true;   // cant start with numbers.
+
+console.log(firstName + " likes Pizza!");
+console.log("and he is " + age + " years old.");
+console.log(canDrive);
+console.log(job);
+
+//
